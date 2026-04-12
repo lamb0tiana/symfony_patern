cf77458bc607fa288b565443268f1152ead232f8
tiana <tiana.randrianarivony@gmail.com>
2026-04-12 12:36:59 +0300

    chore(fix)

--- app/src/Controller/HomeController.php
+++ app/src/Controller/HomeController.php
@@ -1,17 +1,16 @@
 <?php
 
 namespace App\Controller;
-
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-
 final class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
+
         return $this->render('index.html.twig');
     }
 }
--- app/src/Twig/Components/LiveDemo.php
+++ app/src/Twig/Components/LiveDemo.php
@@ -4,17 +4,21 @@ namespace App\Twig\Components;
 
 use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
 use Symfony\UX\LiveComponent\Attribute\LiveAction;
+use Symfony\UX\LiveComponent\Attribute\LiveProp;
 use Symfony\UX\LiveComponent\DefaultActionTrait;
 
 #[AsLiveComponent]
 final class LiveDemo
 {
     use DefaultActionTrait;
+    #[LiveProp]
     public int $number=5;
 
     #[LiveAction]
     public function getData(): int
     {
-        return rand(0, 100  );
+        $this->number = rand(0, 100  );
+
+        return $this->number;
     }
 }
